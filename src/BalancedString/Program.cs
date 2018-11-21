4f67473119ed2c5680f9e817b2115525f401231a
shreesharao <sshreesha4@gmail.com>
2018-11-21 18:13:03 +0530

    initial commit

---
+++ src/BalancedString/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BalancedString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            Console.WriteLine(IsBalancedString(input));
+            Console.Read();
+        }
+
+        private static bool IsBalancedString(string input)
+        {
+            Stack<char> stack = new Stack<char>();
+            char character;
+            foreach (var chr in input)
+            {
+                switch (chr)
+                {
+                    case '(':
+                        stack.Push(chr);
+                        break;
+                    case ')':
+                        character = stack.Pop();
+                        if (character == '(')
+                        {
+                            continue;
+                        }
+                        return false;
+                    case '[':
+                        stack.Push(chr);
+                        break;
+                    case ']':
+                        character = stack.Pop();
+                        if (character == '[')
+                        {
+                            continue;
+                        }
+                        return false;
+
+                    case '<':
+                        stack.Push(chr);
+                        break;
+                    case '>':
+                        character = stack.Pop();
+                        if (character == '<')
+                        {
+                            continue;
+                        }
+                        return false;
+
+                    default:
+                        break;
+                }
+            }
+            if (stack.Count > 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/BalancedString/readme.txt
@@ -0,0 +1,15 @@
+﻿Asked By -
+	Nextgen
+
+Algo-
+        //1) Use a stack
+        //2) read string from left
+        //3) push to stack if current read letter is open brace('(','{','[')
+        //4) pop from stack if current read letter is close brace.
+        //5) Check the popped brace with the current read brace
+        //5.a) if it is pairing brace.ok continue
+        //5.b) if stack was empty, then print NO
+        //5.c) if popped char and read char is not a pair then print NO
+        //6) when all the string are processed.check the length of the stack.
+        //6.a) if length is 0 print YES
+        //6.b) else print NO
